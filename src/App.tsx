8793f2c46ca0779bb08c2f330b097e9b7d3918e3
Michel R. <michel.rousset@adp.com>
2023-11-18 12:14:51 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <Header title="P5JS / Solid" />
       <div class="m-auto flex flex-col justify-between w-[800px] bg-stone-300 h-96">
         <P5Div runner={sampleA()}></P5Div>
+        <div class="bg-stone-100"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, asperiores doloremque debitis impedit, corporis dignissimos mollitia, quam sequi porro corrupti nobis aliquam minus fugiat. Et rem suscipit eveniet odit. Mollitia!</p></div>
         <P5Div runner={sampleB()}></P5Div>
       </div>
     </>
