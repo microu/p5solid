f2d441954c9024b2dbe787756b1101eb827dc813
Michel R. <nobody@microu.org>
2023-11-21 07:23:22 +0100

    sample_itemsGroupB reorder

--- src/p5samples/sample_itemsGroupB.ts
+++ src/p5samples/sample_itemsGroupB.ts
@@ -6,6 +6,112 @@ import { P5Drawer } from "../p5div/P5Drawer";
 import { colorChoices01 } from "./colorChoices";
 import { IP5TimeContext, P5TimeContext } from "../p5div/P5Items";
 import { MaterialPoint } from "../geo2d";
+export function sampleItemsGroupB(): P5Runner {
+  // parameters
+  const bgcolor = resolveColor("pink-950");
+  const nLines = 12;
+
+  // state
+  const context = new P5TimeContext();
+  let nextAppendChild = 0;
+
+  // group + hooks
+  const group = new P5ItemsGroup({ postDraw });
+  function postDraw(ig: P5ItemsGroup, p: p5, ctx: IP5TimeContext) {
+    while (group.length < nLines && ctx.t > nextAppendChild) {
+      ig.appendChild(createLineB0(ctx.t));
+      nextAppendChild = ctx.t + Math.random() * 400;
+    }
+  }
+
+  function setup(p: p5) {
+    p.createCanvas(256, 256);
+    p.frameRate(32);
+  }
+
+  function draw(p: p5) {
+    context.draw(p);
+    p.background(bgcolor);
+    group.draw(p, context);
+  }
+
+  return new P5Runner(setup, draw);
+}
+
+//
+// Lines
+//
+
+function createLineB0(t0: number) {
+  const expiration = t0 + 6000 + 6000 * Math.random();
+  const color =
+    colorChoices01[Math.floor(Math.random() * colorChoices01.length)];
+
+  const ga = (1 / 30_000) * Math.sign(Math.random() - 0.5);
+  const gb = (1 / 30_000) * Math.sign(Math.random() - 0.5);
+  const pa = new MovingPoint(0, Math.random() * 256, {
+    gravity: ga,
+    xmax: 256,
+    ymax: 256,
+  });
+  const pb = new MovingPoint(256, Math.random() * 256, {
+    gravity: gb,
+    xmax: 256,
+    ymax: 256,
+  });
+
+  let size = 0;
+  let expiring = false;
+
+  return new P5Drawer((p, ctx) => {
+    if (size >= 1) {
+      let fxa = ((32 - pa.x) * 1) / 1_000_000;
+      fxa = fxa < 0 ? 10 * fxa : fxa;
+      let fxb = ((224 - pb.x) * 1) / 1_200_000;
+      fxb = fxb > 0 ? 10 * fxb : fxb;
+
+      pa.update(ctx.dt, fxa);
+      pb.update(ctx.dt, fxb);
+    } else {
+      pa.update(ctx.dt);
+      pb.update(ctx.dt);
+    }
+
+    p.stroke(color);
+    p.strokeWeight(2);
+    p.noFill();
+
+    if (!expiring) {
+      if (size >= 1) {
+        size = 1;
+      } else {
+        size += ((-0.1 + Math.random()) * ctx.dt) / 200;
+        size = Math.min(1, size);
+      }
+    } else {
+      size -= ((-0.1 + Math.random()) * ctx.dt) / 300;
+    }
+
+    const k0 = 0.5 - size / 2;
+    const k1 = 0.5 + size / 2;
+    const x0 = pa.x + k0 * (pb.x - pa.x);
+    const x1 = pa.x + k1 * (pb.x - pa.x);
+    const y0 = pa.y + k0 * (pb.y - pa.y);
+    const y1 = pa.y + k1 * (pb.y - pa.y);
+
+    p.line(x0, y0, x1, y1);
+
+    if (ctx.t > expiration) {
+      expiring = true;
+    }
+
+    return expiring && size < 0 ? "!done" : "";
+  });
+}
+
+//
+// Moving Point
+//
 
 type TMovingPointOptions = {
   m: number;
@@ -54,104 +160,4 @@ class MovingPoint extends MaterialPoint {
       this.vy = -Math.abs(this.vy);
     }
   }
-}
-
-function createLineB0(t0: number) {
-  const expiration = t0 + 3000 + 10000 * Math.random();
-  const color =
-    colorChoices01[Math.floor(Math.random() * colorChoices01.length)];
-
-  const ga = (1 / 30_000) * Math.sign(Math.random() - 0.5);
-  const gb = (1 / 30_000) * Math.sign(Math.random() - 0.5);
-
-  const pa = new MovingPoint(0, Math.random() * 256, {
-    gravity: ga,
-    xmax: 256,
-    ymax: 256,
-  });
-  const pb = new MovingPoint(256, Math.random() * 256, {
-    gravity: gb,
-    xmax: 256,
-    ymax: 256,
-  });
-
-  let size = 0;
-  let expiring = false;
-
-  return new P5Drawer((p, ctx) => {
-    if (size >=1) {
-
-      let fxa = (32 - pa.x) * 1/1_000_000
-      fxa = fxa < 0 ? 10*fxa : fxa
-      let fxb = (224 - pb.x) * 1/1_200_000
-      fxb = fxb > 0 ? 10*fxb : fxb
-
-      pa.update(ctx.dt, fxa);
-      pb.update(ctx.dt, fxb);
-    } else  {
-      pa.update(ctx.dt);
-      pb.update(ctx.dt);    
-    }
-
-    p.stroke(color);
-    p.strokeWeight(2);
-    p.noFill();
-
-    if (!expiring) {
-      if (size >= 1) {
-        size = 1;
-      } else {
-        size += ((-0.1 + Math.random()) * ctx.dt) / 200;
-        size = Math.min(1, size);
-      }
-    } else {
-      size -= ((-0.1 + Math.random()) * ctx.dt) / 300;
-    }
-
-    const k0 = 0.5 - size / 2;
-    const k1 = 0.5 + size / 2;
-    const x0 = pa.x + k0 * (pb.x - pa.x);
-    const x1 = pa.x + k1 * (pb.x - pa.x);
-    const y0 = pa.y + k0 * (pb.y - pa.y);
-    const y1 = pa.y + k1 * (pb.y - pa.y);
-
-    p.line(x0, y0, x1, y1);
-
-    if (ctx.t > expiration) {
-      expiring = true;
-    }
-
-    return expiring && size < 0 ? "!done" : "";
-  });
-}
-
-export function sampleItemsGroupB(): P5Runner {
-  // parameters
-  const bgcolor = resolveColor("pink-950");
-
-  // state
-  const context = new P5TimeContext();
-  const nLines = 12;
-
-  // group + hooks
-  function postDraw(ig: P5ItemsGroup, p: p5, ctx: IP5TimeContext) {
-    while (group.length < nLines) {
-      ig.appendChild(createLineB0(ctx.t));
-    }
-  }
-
-  const group = new P5ItemsGroup({ postDraw });
-
-  function setup(p: p5) {
-    p.createCanvas(256, 256);
-    p.frameRate(32);
-  }
-
-  function draw(p: p5) {
-    context.draw(p);
-    p.background(bgcolor);
-    group.draw(p, context);
-  }
-
-  return new P5Runner(setup, draw);
 }
