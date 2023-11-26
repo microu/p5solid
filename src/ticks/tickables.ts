95cb093c5a4c8fff08b3a1e5b132ea8f20871edf
michelr <noreply@microu.org>
2023-11-26 15:33:05 +0100

    ...

--- src/ticks/tickables.ts
+++ src/ticks/tickables.ts
@@ -8,6 +8,11 @@ export interface IClock {
   dt: number;
 }
 
+export interface IParentClock {
+  addChild(child: ITickable): void;
+  removeChild(child: ITickable): void;
+}
+
 type TParentClockOptions = {
   t0?: number;
   scale: number;
@@ -18,37 +23,49 @@ const DEFAULT_TParentClockOptions: TParentClockOptions = {
   scale: 1,
 };
 
-export class ParentClock implements ITickable, IClock {
+export class ParentClock implements ITickable, IClock, IParentClock {
   private _t: number;
   private _dt: number;
   private _state: "" | "initialized" | "started" = "";
   private _parent_t = 0;
   private t0: number = 0;
+  private _children: ITickable[] = [];
+
   options: { t0?: number | undefined; scale: number };
 
   constructor(options: Partial<TParentClockOptions> = {}) {
     this.options = { ...DEFAULT_TParentClockOptions, ...options };
     this._t = 0;
-    this._dt = -1;
+    this._dt = 0;
   }
 
-  tick(t: number, dt: number): void {
-      if ( this._state == "") {
-        this.t0 = this.options.t0 ?? t
-        this._state = "initialized"
-      }
+  addChild(child: ITickable): void {
+    this._children.push(child);
+  }
+  removeChild(child: ITickable): void {
+    const pos = this._children.indexOf(child);
+    if (pos >= 0) {
+      this._children.splice(pos, 1);
+    }
+  }
 
-      if (this.started && t <= this._parent_t) {
-        throw new Error(
-          `Tick times must be stricly increasing : ${t} <=  ${this._parent_t}`
-        );
-      }
+  tick(t: number, _dt: number): void {
+    if (this._state == "") {
+      this.t0 = this.options.t0 ?? t;
+      this._state = "initialized";
+    }
 
-      if (t >= this.t0) {      
-        const ct = (t - this.t0) / this.options.scale;
-        const dct = ct- this._t;
-        this._update(t, ct, dct)
-      }
+    if (this.started && t <= this._parent_t) {
+      throw new Error(
+        `Tick times must be stricly increasing : ${t} <=  ${this._parent_t}`
+      );
+    }
+
+    if (t >= this.t0) {
+      const ct = (t - this.t0) * this.options.scale;
+      const dct = ct - this._t;
+      this._update(t, ct, dct);
+    }
   }
 
   private _update(parent_t: number, t: number, dt: number) {
@@ -56,6 +73,10 @@ export class ParentClock implements ITickable, IClock {
     this._parent_t = parent_t;
     this._t = t;
     this._dt = dt;
+
+    for (const child of this._children) {
+      child.tick(this.t, this.dt);
+    }
   }
 
   get t() {
