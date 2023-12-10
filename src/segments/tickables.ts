e70f7342e0d3d89a0fa746239d8cdd3508b37307
Michel R. <michel.rousset@adp.com>
2023-12-10 18:29:49 +0100

    Introducing tickables (from step0)

---
+++ src/segments/tickables.ts
@@ -0,0 +1,143 @@
+export interface ITickable {
+  tick(t: number): void;
+}
+
+export interface IClock {
+  readonly started: boolean;
+  readonly t: number;
+  paused: boolean;
+}
+
+export interface IParentClock extends IClock {
+  addChild(child: ITickable): void;
+  removeChild(child: ITickable): void;
+}
+type TClockBaseOptions = {
+  tick0?: number;
+  // origin in ticker scale (t0 in clock scale).
+  // if undefined will be thetime of first tick
+  t0: number;
+  // t0 in ticker scale (0 in clock scale).
+  // if undefined will be thetime of first tick
+
+  scale: number;
+  // time scale
+};
+const DEFAULT_TClockBaseOptions: TClockBaseOptions = {
+  tick0: undefined,
+  t0: 0,
+  scale: 1,
+};
+
+export class ClockBase implements IClock, ITickable {
+  private _state:
+    | ""
+    | "initialized"
+    | "started"
+    | "pause_requested"
+    | "paused"
+    | "unpaused" = "";
+
+  private _tick0: number = 0; // clock origin (ticker scale)
+  private _t0: number = 0; // clock origin (clock scale)
+  private _scale: number = 1;
+
+  private _t: number = 0; // current time (clock scale) if _started
+  private options: TClockBaseOptions;
+
+  constructor(options: Partial<TClockBaseOptions> = {}) {
+    this.options = { ...DEFAULT_TClockBaseOptions, ...options };
+    this._scale = this.options.scale;
+  }
+
+  tick(t: number): void {
+    if (this._state == "") {
+      this._tick0 = this.options.tick0 ?? t;
+      this._t0 = this.options.t0;
+      this._state = "initialized";
+    }
+
+    if (this._state == "unpaused") {
+      this._tick0 = t;
+      this._t0 = this._t;
+      this._state = "started";
+    }
+
+    if (this._state == "pause_requested") {
+      this._state = "paused";
+    }
+
+    if (t >= this._tick0) {
+      const new_t = this._t0 + (t - this._tick0) * this._scale;
+      if (this.started && new_t < this.t) {
+        throw new Error(
+          `Tick times must be increasing but  ${new_t} <  ${this._t}`
+        );
+      }
+      if (!this.paused) {
+        this._t = new_t;
+        this._state = "started";
+      }
+    }
+  }
+
+  get t() {
+    return this._t;
+  }
+
+  get started() {
+    return this._state != "" && this._state != "initialized";
+  }
+
+  get paused() {
+
+    return this._state == "paused" || this._state == "pause_requested";
+  }
+
+  set paused(v: boolean) {
+    if (this.paused == v) return;
+
+    if (v) {
+      this._state = "pause_requested";
+    } else {
+      this._state = this._state == "pause_requested" ? "started" : "unpaused";
+    }
+
+  }
+
+  get state() {
+    return this._state;
+  }
+}
+
+export type TParentClockOptions = TClockBaseOptions;
+
+export class ParentClock
+  extends ClockBase
+  implements ITickable, IParentClock, IClock
+{
+  private _children: ITickable[] = [];
+
+  constructor(options: Partial<TParentClockOptions> = {}) {
+    super(options);
+  }
+
+
+  tick(t: number): void {
+    super.tick(t)
+    for (const child of this._children) {
+      child.tick(this.t)
+    }
+  }
+
+
+  addChild(child: ITickable): void {
+    this._children.push(child);
+  }
+  removeChild(child: ITickable): void {
+    const pos = this._children.indexOf(child);
+    if (pos >= 0) {
+      this._children.splice(pos, 1);
+    }
+  }
+}
---
+++ test/segments/clockBase.test.ts
@@ -0,0 +1,228 @@
+import { expect, test, describe, it } from "vitest";
+import { ClockBase } from "@src/segments/tickables";
+
+describe("New ClockBase", function () {
+  const cb1 = new ClockBase();
+  const cb2 = new ClockBase({ tick0: 1000 });
+  it("is not started and not paused", function () {
+    expect(cb1.started).toBe(false);
+    expect(cb1.paused).toBe(false);
+    expect(cb2.started).toBe(false);
+    expect(cb2.paused).toBe(false);
+  });
+
+  it("is started after tick0", function () {
+    cb1.tick(0);
+    expect(cb1.started).toBe(true);
+    expect(cb1.paused).toBe(false);
+    expect(cb1.t).toBe(0);
+
+    cb2.tick(0);
+    expect(cb2.started).toBe(false);
+    expect(cb2.paused).toBe(false);
+
+    cb2.tick(1100);
+    expect(cb2.started).toBe(true);
+    expect(cb2.paused).toBe(false);
+    expect(cb2.t).toBe(100);
+  });
+});
+
+describe("Running ClockBase", function () {
+  test("ClockBase with option scale", function () {
+    const cb = new ClockBase({ scale: 10 });
+
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+
+    cb.tick(1000);
+    expect(cb.started).toBe(true);
+    expect(cb.t).toBe(0);
+
+    cb.tick(1001);
+    expect(cb.started).toBe(true);
+    expect(cb.t).toBe(10);
+
+    cb.tick(1001.1);
+    expect(cb.started).toBe(true);
+    expect(cb.t).toBeCloseTo(11);
+
+    cb.tick(1010);
+    expect(cb.started).toBe(true);
+    expect(cb.t).toBeCloseTo(100);
+  });
+
+  test("ClockBase with option tick0", function () {
+    const cb = new ClockBase({ tick0: 10000 });
+
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0);
+
+    cb.tick(0);
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0);
+
+    cb.tick(5000);
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0);
+
+    cb.tick(9000);
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0);
+
+    cb.tick(9999);
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0);
+
+    cb.tick(10000);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0);
+
+    cb.tick(10010);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(10);
+
+    cb.tick(20000);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(10000);
+  });
+
+  test("ClockBase with option t0", function () {
+    const cb = new ClockBase({ t0: 1000 });
+
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0);
+
+    cb.tick(0);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(1000);
+
+    cb.tick(1);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(1001);
+
+    cb.tick(5000);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(6000);
+
+    cb.tick(20000);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(21000);
+  });
+
+  test("ClockBase with options t0, tick0 adnd scale", function () {
+    const cb = new ClockBase({ tick0: 100, t0: 1000, scale: 2 });
+
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0);
+
+    cb.tick(0);
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0);
+
+    cb.tick(99.9);
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0);
+
+    cb.tick(101);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(1002);
+
+    cb.tick(102);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(1004);
+
+    cb.tick(110);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(1020);
+
+    cb.tick(300);
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(1400);
+  });
+});
+
+describe("Running/pausing ClockBase", function () {
+  test("Basic run pause", function () {
+    const cb = new ClockBase();
+    expect(cb.started).toBe(false);
+    expect(cb.paused).toBe(false);
+
+    cb.tick(1000)
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(0)
+
+    cb.tick(2000)
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(1000)
+
+    cb.paused = true
+    
+    cb.tick(3000)
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(true);
+    expect(cb.t).toBe(1000)
+    
+    cb.tick(3500)
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(true);
+    expect(cb.t).toBe(1000)
+    
+    cb.tick(3999)
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(true);
+    expect(cb.t).toBe(1000)
+    
+    cb.paused = false;
+    
+    cb.tick(4000)
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(1000)
+
+    cb.tick(5000)
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(2000)
+
+    // pausing unpausing between 2 ticks as no effect
+    cb.paused = true
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(true);
+    expect(cb.t).toBe(2000)
+
+    cb.paused = false
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(2000)
+
+    cb.tick(6000)
+    expect(cb.started).toBe(true);
+    expect(cb.paused).toBe(false);
+    expect(cb.t).toBe(3000)
+
+
+  });
+});
