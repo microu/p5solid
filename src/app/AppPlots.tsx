357d96da45a8655f2a9b0d1b418f39e4619d46b5
Michel R. <michel.rousset@adp.com>
2023-12-17 09:37:48 +0100

    ...

--- src/app/AppPlots.tsx
+++ src/app/AppPlots.tsx
@@ -1,48 +1,12 @@
 import Header from "./Header";
 import * as Plot from "@observablehq/plot";
+import { plotSinPeriodOffset } from "./plotSamples/sinPeriodOffset";
+import { plotSinKeyPoints } from "./plotSamples/sinKeyPoint";
 
-import { PVSin } from "@src/pvalue/PVSin";
-
-type TPlotData = {
-  x: number;
-  y: number;
-  name: string;
-};
-
-const data = [] as TPlotData[];
-const xa = -10;
-const xb = 10;
-const xdelta = xb - xa;
-
-const pvsin = [] as PVSin[];
-
-pvsin.push(new PVSin({ min: -10, max: 10, period: 2 }));
-pvsin.push(new PVSin({ min: -10, max: 10, period: 2, periodShift: 0.1 }));
-pvsin.push(new PVSin({ min: -10, max: 10, period: 2, periodShift: 0.3 }));
-
-for (let i = 0; i <= 1000; i += 1) {
-  const x = xa + (i * xdelta) / 1000;
-  for (let j = 0; j < pvsin.length; j += 1) {
-    const r: TPlotData = { x, y: pvsin[j].v(x), name: `y${j + 1}` };
-    data.push(r);
-  }
-}
-console.log(data);
 
 function AppPlots() {
   let plotDiv: HTMLDivElement | undefined;
 
-  const marks = [Plot.line(data, { x: "x", y: "y", stroke: "name" })];
-
-  const plot = Plot.plot({
-    width: 500,
-    height: 250,
-    style: "overflow: visible;",
-    color: { type: "categorical", scheme: "dark2", legend: true },
-    x: { grid: true },
-    y: { grid: true },
-    marks,
-  });
 
   const r = (
     <>
@@ -53,7 +17,7 @@ function AppPlots() {
       </div>
     </>
   );
-  plotDiv!.append(plot);
+  plotDiv!.append(plotSinKeyPoints());
   return r;
 }
 
